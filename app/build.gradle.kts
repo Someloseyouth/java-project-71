22dd2b6dbff9caefb2562d718b4a7dcc68a25cb1
someloseyouth <valeriknikulin@gmail.com>
2025-06-27 13:33:33 +0300

    Reading and parsing files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,10 @@ application { mainClass.set("hexlet.code.App") }
 repositories { mavenCentral() }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.1")
+    implementation("info.picocli:picocli:4.7.7")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,44 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.concurrent.Callable;
+import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "Path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "Path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format = "stylish";
+
+    @Override
+    public Integer call() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> data1 = objectMapper.readValue(Files.readString(Paths.get(filepath1)), Map.class);
+        Map<String, Object> data2 = objectMapper.readValue(Files.readString(Paths.get(filepath2)), Map.class);
+
+        System.out.println("File 1: " + data1);
+        System.out.println("File 2: " + data2);
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }

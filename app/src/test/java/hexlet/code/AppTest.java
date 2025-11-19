a8c19e6d1d5ead6bb98d5bd313edcb979b832bce
someloseyouth <valeriknikulin@gmail.com>
2025-11-19 20:18:25 +0300

    add AppTest

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class AppTest {
+
+    @Test
+    void testRunWithDefaultFormat() {
+        String[] args = {"src/test/resources/fixtures/filepath1.json", "src/test/resources/fixtures/filepath2.json"};
+        int exitCode = new CommandLine(new App()).execute(args);
+        assertEquals(0, exitCode);
+    }
+
+    @Test
+    void testRunWithFormat() {
+        String[] args = {"src/test/resources/fixtures/filepath1.json",
+            "src/test/resources/fixtures/filepath2.json", "-f", "plain"};
+        int exitCode = new CommandLine(new App()).execute(args);
+        assertEquals(0, exitCode);
+    }
+
+    @Test
+    void testRunWithUnknownFormat() {
+        String[] args = {"src/test/resources/fixtures/filepath1.json",
+            "src/test/resources/fixtures/filepath2.json", "-f", "exe"};
+        int exitCode = new CommandLine(new App()).execute(args);
+        assertNotEquals(0, exitCode);
+    }
+}
